ed12a360d75fdac7a6bd4b0ba4d8bbe115fab846
navya <navyasreeb45@gmail.com>
2026-06-10 15:33:26 +0530

    "Fix: Secured backend API endpoint using HTTPS"

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://communityconnect-backend-biqt.onrender.com' || 'http://127.0.0.1:8000/api',
+  // baseURL: 'https://communityconnect-backend-biqt.onrender.com' || 'http://127.0.0.1:8000/api',
+  baseURL: 'https://communityconnect-backend-biqt.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
