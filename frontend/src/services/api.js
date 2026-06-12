d5b2e2b6928a8825145c6be81f00a4c088669aca
navya <navyasreeb45@gmail.com>
2026-06-12 15:56:00 +0530

    bakend url changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const api = axios.create({
   // baseURL: 'https://communityconnect-backend-biqt.onrender.com' || 'http://127.0.0.1:8000/api',
   // baseURL: 'https://communityconnect-backend-biqt.onrender.com',
-  baseURL: import.meta.env.VITE_API_URL, 
+  // baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://127.0.0.1:8000/api',
   withCredentials: true,
 });
 
